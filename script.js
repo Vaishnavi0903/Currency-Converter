d5aa7aa1cbf1f39d2488e0672c12d717012b54ee
Vaishnavi Sontakke <vaishnavisontakke0903@gmail.com>
2024-01-12 13:36:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const baseURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for(let select of dropdowns)
+{
+    for(currCode in countryList)
+    {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode === "USD")
+        {
+            newOption.selected = "selected";
+        }
+        if(select.name === "to" && currCode === "INR")
+        {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change" , (evt)=>
+    {
+        updateFlag(evt.target);
+        //target : where the change has occured
+    })
+}
+
+const updateFlag = (element) =>
+{
+     let currCode = element.value;
+     let countryCode = countryList[currCode];
+     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+     const img = element.parentElement.querySelector("img");
+     img.src = newSrc;
+}
+
+btn.addEventListener("click" , async (evt) =>
+{
+    evt.preventDefault();
+    //to prevent automatic behaviour(say refresh) on clicking button
+    const amount = document.querySelector(".amount input");
+    let amtValue = amount.value;
+    if(amtValue === "" || amtValue < 1)
+    {
+        amtValue = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${baseURL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+
+    let finalAmt = amtValue * rate;
+    msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+})
